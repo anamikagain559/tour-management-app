346795512f660b47504fe8fae5bbbe7e7ff66e08
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-08-30 11:35:42 +0600

    user register done

--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -36,6 +36,8 @@ export function LoginForm({
     }
   };
 
+
+  
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
