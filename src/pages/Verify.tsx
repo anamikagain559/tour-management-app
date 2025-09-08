0b45a6396c29452942154ea60a162d62ab64aabb
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-09-08 16:16:03 +0600

    otp functionality ongoing

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -1,20 +1,89 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
-
+import { useForm } from "react-hook-form"
+import {
+  Card,
+  CardAction,
+  
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button";
 export default function Verify() {
   const location = useLocation();
   const navigate = useNavigate();
   const [email] = useState(location.state);
 
-  useEffect(() => {
-    if (!email) {
-      navigate("/");
-    }
-  }, [email]);
+
+  // useEffect(() => {
+  //   if (!email) {
+  //     navigate("/");
+  //   }
+  // }, [email]);
+
+
 
   return (
     <div>
-      <h1> This is Verify component </h1>
+<Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+     <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="pin"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>One-Time Password</FormLabel>
+              <FormControl>
+                <InputOTP maxLength={6} {...field}>
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </FormControl>
+              <FormDescription>
+                Please enter the one-time password sent to your phone.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
     </div>
   );
 }
